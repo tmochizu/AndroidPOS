eaf8f6778d8d25b2c626e0a8955dce262f62761c
Takuya Mizuhara <z8x8c.zeolite.m@gmail.com>
2013-12-23 14:57:12 +0900

    Fix merge bug

--- src/com/ricoh/pos/MainMenuActivity.java
+++ src/com/ricoh/pos/MainMenuActivity.java
@@ -40,6 +40,17 @@ public class MainMenuActivity extends Activity {
 					}
 				});
 
+		findViewById(R.id.SalesButton).setOnClickListener(
+				new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						Intent intent = new Intent().setClass(
+								MainMenuActivity.this,
+								SalesCalenderActivity.class);
+						startActivity(intent);
+					}
+				});
+
 		findViewById(R.id.SyncButton).setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
