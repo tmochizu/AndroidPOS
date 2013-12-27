46e7f779a1e6ae2079fe8a56c84a0b6504575e95
Tetsuro Kutsuwada <tetsuroh.kutsuwada@nts.ricoh.co.jp>
2013-12-27 20:20:55 +0900

    add order and RegisterManager

---
+++ src/com/ricoh/pos/model/RegisterManager.java
@@ -0,0 +1,51 @@
+package com.ricoh.pos.model;
+
+import java.util.ArrayList;
+
+import com.ricoh.pos.data.Product;
+import com.ricoh.pos.data.Order;
+
+public class RegisterManager {
+	
+	private ArrayList<Order> orderList;
+	
+	public RegisterManager(){
+		orderList = new ArrayList<Order>();
+	}
+	
+	public void updateOrder(Product product, int num){
+		Order orderOfTheProduct = findOrderOfTheProduct(product);
+		
+		if (orderOfTheProduct == null) {
+			Order newOrder = new Order(product,num);
+			orderList.add(newOrder);
+		} else {
+			orderOfTheProduct.setNumberOfOrder(num);
+		}
+	}
+	
+	public int getTotalAmount(){
+		int totalAmount = 0;
+		for (Order order: orderList) {
+			totalAmount += order.getTotalAmount();
+		}
+		return totalAmount;
+	}
+	
+	public void clearAllOrders(){
+		orderList = new ArrayList<Order>();
+	}
+	
+	private Order findOrderOfTheProduct(Product product){
+		for (Order order : orderList) {
+			if (order.equals(product.getName())) {
+				return order;
+			}
+		}
+		// Not Found
+		return null;
+	}
+	
+
+
+}
