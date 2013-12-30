3dbf96895ed571bd175c2b4a6f5c9b48fde42194
Tetsuro Kutsuwada <tetsuroh.kutsuwada@nts.ricoh.co.jp>
2013-12-30 14:28:52 +0900

    fix review indication

--- src/com/ricoh/pos/data/Product.java
+++ src/com/ricoh/pos/data/Product.java
@@ -18,18 +18,38 @@ public class Product {
 	// Setter
 	///////////////////////////
 	public void setOriginalCost(int cost){
+		
+		if (cost <= 0) {
+			throw new IllegalArgumentException("Original cost should be over zero");
+		}
+		
 		this.originalCost = cost;
 	}
 	
 	public void setPrice(int price){
+		
+		if (price <= 0) {
+			throw new IllegalArgumentException("Price should be over zero");
+		}
+		
 		this.price = price;
 	}
 	
 	public void setStock(int stock){
+		
+		if (stock < 0) {
+			throw new IllegalArgumentException("Stock should be positive");
+		}
+		
 		this.stock = stock;
 	}
 	
 	public void setProductImagePath(String imagePath){
+		
+		if (imagePath == null || imagePath.length() == 0) {
+			throw new IllegalArgumentException("Passing imagePath is not valid");
+		}
+		
 		this.imagePath = imagePath;
 	}
 	
@@ -62,8 +82,4 @@ public class Product {
 		return imagePath;
 	}
 	
-	
-	public Product clone(){
-		return this.clone();
-	}
 }
