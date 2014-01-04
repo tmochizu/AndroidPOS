dfedcd06600e7511abd7906fd5b9048ed4d0ffdb
kNishide <kohichi.nishide@nts.ricoh.co.jp>
2014-01-04 22:27:20 +0900

    refactor

--- AndroidPOS/src/com/ricoh/pos/TotalPaymentActivity.java
+++ AndroidPOS/src/com/ricoh/pos/TotalPaymentActivity.java
@@ -1,10 +1,10 @@
 package com.ricoh.pos;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
-public class TotalPaymentActivity extends FragmentActivity{
-
+public class TotalPaymentActivity extends FragmentActivity implements TotalPaymentFragment.OnOkButtonClickListener{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -17,5 +17,10 @@ public class TotalPaymentActivity extends FragmentActivity{
 			.add(R.id.total_payment_container, fragment).commit();
 		}
 		// TODO: If exposing deep links into your app, handle intents here.
+	}
+
+	@Override
+	public void onOkClicked() {
+		startActivity(new Intent(this, RegisterConfirmActivity.class));
 	}
 }
--- AndroidPOS/src/com/ricoh/pos/TotalPaymentFragment.java
+++ AndroidPOS/src/com/ricoh/pos/TotalPaymentFragment.java
@@ -2,7 +2,7 @@ package com.ricoh.pos;
 
 import java.text.NumberFormat;
 
-import android.content.Intent;
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -16,42 +16,58 @@ import com.ricoh.pos.model.RegisterManager;
 import com.ricoh.pos.model.UpdateOrderListener;
 
 public class TotalPaymentFragment extends Fragment implements UpdateOrderListener{
-	
+
 	// This is the maximum fraction digits for total payment to display.
 	private static final int MAXIMUM_FRACTION_DIGITS = 2;
-	
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_total_payment, container, false);
-        
-        Button ok_button = (Button) v.findViewById(R.id.total_payment_ok_button);
-        ok_button.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				startActivity(new Intent(getActivity(), RegisterConfirmActivity.class));
-			}
-		});
-        return v;
-    }
-	
+	private OnOkButtonClickListener okButtonClickListener; 
+
+	@Override  
+	public void onAttach(Activity activity) {  
+		super.onAttach(activity); 
+		if (activity instanceof OnOkButtonClickListener == false) {  
+			throw new ClassCastException("okButtonClickListener isn't implemented");  
+		}  
+		okButtonClickListener = (OnOkButtonClickListener) activity;  
+	}
+
+	@Override  
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		RegisterManager.getInstance().setUpdateOrderListener(this);
 	}
-    
+
+	@Override  
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_total_payment, container, false);
+
+		Button ok_button = (Button) v.findViewById(R.id.total_payment_ok_button);
+		ok_button.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (okButtonClickListener != null) {  
+					okButtonClickListener.onOkClicked();  
+				}
+			}
+		});
+		return v;
+	}
+
 	@Override
 	public void notifyUpdateOrder(double totalPayment) {
 		TextView totalPaymentView = (TextView) getView().findViewById(R.id.totalPaymentView);
-		
+
 		NumberFormat format = NumberFormat.getInstance();
 		format.setMaximumFractionDigits(MAXIMUM_FRACTION_DIGITS);
 		totalPaymentView.setText(format.format(totalPayment) + " Rp");
 	}
-	
+
 	@Override
 	public void onDestroy(){
 		super.onDestroy();
-		
 		RegisterManager.getInstance().clearUpdateOrderListener();
 	}
 
+	public interface OnOkButtonClickListener {  
+		public void onOkClicked();  
+	}  
 }
