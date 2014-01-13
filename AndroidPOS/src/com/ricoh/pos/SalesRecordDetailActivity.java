89f5ef6e6d3f89d9329e169c47d6817860abb111
Tetsuro Kutsuwada <tetsuroh.kutsuwada@nts.ricoh.co.jp>
2014-01-13 18:01:03 +0900

    implemtn sales record UI

---
+++ AndroidPOS/src/com/ricoh/pos/SalesRecordDetailActivity.java
@@ -0,0 +1,55 @@
+package com.ricoh.pos;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
+
+public class SalesRecordDetailActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_category_detail);
+
+		// Show the Up button in the action bar.
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+
+		// savedInstanceState is non-null when there is fragment state
+		// saved from previous configurations of this activity
+		// (e.g. when rotating the screen from portrait to landscape).
+		// In this case, the fragment will automatically be re-added
+		// to its container so we don't need to manually add it.
+		// For more information, see the Fragments API guide at:
+		//
+		// http://developer.android.com/guide/components/fragments.html
+		//
+		if (savedInstanceState == null) {
+			// Create the detail fragment and add it to the activity
+			// using a fragment transaction.
+			SalesRecordDetailFragment fragment = new SalesRecordDetailFragment();;
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.category_detail_container, fragment).commit();
+		}
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpTo(this, new Intent(this,
+					CategoryListActivity.class));
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+}
---
+++ AndroidPOS/src/com/ricoh/pos/SalesRecordDetailFragment.java
@@ -0,0 +1,111 @@
+package com.ricoh.pos;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.ricoh.pos.data.Order;
+import com.ricoh.pos.data.Product;
+import com.ricoh.pos.data.SingleSalesRecord;
+import com.ricoh.pos.model.SalesCalenderManager;
+import com.ricoh.pos.model.SalesRecordManager;
+
+public class SalesRecordDetailFragment extends ListFragment {
+	
+	private ArrayList<Order> orders;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		Date date = SalesCalenderManager.getInstance().getSelectedDate();
+		
+		//TODO
+		//SingleSalesRecord salesRecord = SalesRecordManager.getInstance().getSingleSalesRecord(date);
+		SingleSalesRecord salesRecord = SalesRecordManager.getInstance().restoreSingleSalesRecordsOfTheDay(date).get(0);
+		
+		orders = salesRecord.getAllOrders();
+		
+		setListAdapter(new ListAdapter(getActivity()));
+		getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+	}
+
+	public class ListAdapter extends BaseAdapter {
+		private LayoutInflater inflater;
+
+		public ListAdapter(Context context) {
+			inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		}
+
+		@Override
+		public int getCount() {
+			return orders.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return 0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			if (convertView == null) {
+				convertView = inflater.inflate(R.layout.order_row, null);
+			}
+			
+			Order order = orders.get(position);
+			Product product = order.getProduct();
+
+			ImageView imageView = (ImageView) convertView.findViewById(R.id.photo);
+			imageView.setLayoutParams(new LinearLayout.LayoutParams(120, 120));
+			imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+			imageView.setImageResource(getResourceID(product.getProductImagePath()));
+
+			TextView textView = (TextView) convertView.findViewById(R.id.filename);
+			textView.setPadding(10, 0, 0, 0);
+			String productName = product.getName();
+			if (productName == null || productName.length() == 0) {
+				throw new NullPointerException("Product name is not valid");
+			}
+			textView.setText(productName);
+
+			TextView priceView = (TextView) convertView.findViewById(R.id.price);
+			priceView.setPadding(10, 0, 0, 0);
+			priceView.setText(String.valueOf(product.getPrice()));
+
+			TextView numberOfSalseView = (TextView) convertView.findViewById(R.id.numberOfSales);
+			numberOfSalseView.setPadding(10, 0, 0, 0);
+
+			if (order == null || order.getNumberOfOrder() == 0) {
+				throw new AssertionError("Product which isn't ordered is shown");
+			} else {
+				int numberOfSales = order.getNumberOfOrder();
+				numberOfSalseView.setText(String.valueOf(numberOfSales));
+			}
+
+			return convertView;
+		}
+
+		private int getResourceID(String fileName) {
+			int resID = getResources().getIdentifier(fileName, "drawable", "com.ricoh.pos");
+			return resID;
+		}
+	}
+
+}
