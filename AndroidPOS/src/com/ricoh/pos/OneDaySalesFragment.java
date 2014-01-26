24278fbd09256954e6a6ff71710191d7b16f1367
Tetsuro Kutsuwada <tetsuroh.kutsuwada@nts.ricoh.co.jp>
2014-01-26 23:44:05 +0900

    add one day sales and revenue

---
+++ AndroidPOS/src/com/ricoh/pos/OneDaySalesFragment.java
@@ -0,0 +1,40 @@
+package com.ricoh.pos;
+
+import java.text.NumberFormat;
+import java.util.Date;
+
+import com.ricoh.pos.data.SingleSalesRecord;
+import com.ricoh.pos.model.SalesCalenderManager;
+import com.ricoh.pos.model.SalesRecordManager;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class OneDaySalesFragment extends Fragment {
+
+	@Override  
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_oneday_sales, container, false);
+		
+		Date date = SalesCalenderManager.getInstance().getSelectedDate();
+
+		TextView oneDaySalesView = (TextView) v.findViewById(R.id.oneDaySales);
+		double  oneDaySales = SalesRecordManager.getInstance().getOneDayTotalSales(date);
+
+		TextView oneDayRevenueView = (TextView) v.findViewById(R.id.oneDayRevenue);
+		double  oneDayRevenue = SalesRecordManager.getInstance().getOneDayTotalRevenue(date);
+
+		NumberFormat format = NumberFormat.getInstance();
+		format.setMaximumFractionDigits(2);
+		
+		oneDaySalesView.setText(format.format(oneDaySales) + getString(R.string.currency_india));
+		oneDayRevenueView.setText(format.format(oneDayRevenue) + getString(R.string.currency_india));
+		
+		return v;
+	}
+	
+}
