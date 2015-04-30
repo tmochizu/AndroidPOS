2d5e74d8fb207aca317bc76293d9198469ff4e39
dnaka <daisuke.nakagawa@jrits.ricoh.co.jp>
2015-04-30 14:52:37 +0900

    BOP-1の障害対応。画像が表示されなくなっていたのを修正

--- AndroidPOS/src/com/ricoh/pos/model/WomanShopSalesIOManager.java
+++ AndroidPOS/src/com/ricoh/pos/model/WomanShopSalesIOManager.java
@@ -119,7 +119,7 @@ public class WomanShopSalesIOManager {
     {
         return "select "
                 + combinedColumns[0] + ", "
-                + combinedColumns[1] + ", "
+                + combinedColumns[1] + ", "0
                 + combinedColumns[2] + ", "
                 + combinedColumns[3] + ", "
                 + combinedColumns[4] + ", "
@@ -173,6 +173,7 @@ public class WomanShopSalesIOManager {
                 }
 
                 Product product = new Product(cursor.getString(3), cursor.getString(4), cursor.getString(5));
+                product.setProductImagePath(cursor.getString(3));
                 product.setOriginalCost(cursor.getDouble(6));
                 product.setPrice(cursor.getDouble(7));
                 Order order = new Order(product, cursor.getInt(8));
@@ -262,6 +263,7 @@ public class WomanShopSalesIOManager {
                 }
 
                 Product product = new Product(cursor.getString(3), cursor.getString(4), cursor.getString(5));
+                product.setProductImagePath(cursor.getString(3));
                 product.setOriginalCost(cursor.getDouble(6));
                 product.setPrice(cursor.getDouble(7));
                 Order order = new Order(product, cursor.getInt(8));
