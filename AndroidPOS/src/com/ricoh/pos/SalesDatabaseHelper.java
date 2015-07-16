7b2634f48aa47de5309ea65fb304cc10af2241a9
arai-wa <wataru.arai@jrits.ricoh.co.jp>
2015-07-16 13:30:55 +0900

    BOP-53 アプリがバージョンアップすると DB もバージョンアップするようにする

--- AndroidPOS/src/com/ricoh/pos/SalesDatabaseHelper.java
+++ AndroidPOS/src/com/ricoh/pos/SalesDatabaseHelper.java
@@ -1,12 +1,18 @@
 package com.ricoh.pos;
 
- import android.content.Context;
- import android.database.sqlite.SQLiteDatabase;
- import android.database.sqlite.SQLiteOpenHelper;
- import android.util.Log;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
- import com.ricoh.pos.data.WomanShopSalesDef;
- import com.ricoh.pos.data.WomanShopSalesOrderDef;
+import com.ricoh.pos.data.Order;
+import com.ricoh.pos.data.SingleSalesRecord;
+import com.ricoh.pos.data.WomanShopSalesDef;
+import com.ricoh.pos.data.WomanShopSalesOrderDef;
+import com.ricoh.pos.model.WomanShopSalesIOManager;
+
+import java.util.ArrayList;
 
 
 /**
@@ -14,40 +20,67 @@ package com.ricoh.pos;
  * ２つのテーブルを生成する。
  */
 public class SalesDatabaseHelper extends SQLiteOpenHelper {
-    public static final String SALES_DB_NAME = "sales.db";
-    public static final String WS_SALES_TABLE_NAME = "tbl_ws_sales";
-    public static final String WS_SALES_ORDER_TABLE_NAME = "tbl_ws_sales_order";
+	public static final String SALES_DB_NAME = "sales.db";
+	public static final String WS_SALES_TABLE_NAME = "tbl_ws_sales";
+	public static final String WS_SALES_ORDER_TABLE_NAME = "tbl_ws_sales_order";
 
-    public SalesDatabaseHelper(Context context) {
-        super(context, SALES_DB_NAME, null, 1);
-    }
+	public SalesDatabaseHelper(Context context) {
+		super(context, SALES_DB_NAME, null, 2);
+	}
 
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL("Create Table " + WS_SALES_TABLE_NAME + " ("
-                        + "_id Integer Primary Key, "
-                        + WomanShopSalesDef.DATE.name() + " Text, "
-                        + WomanShopSalesDef.DISCOUNT.name() + " Real, "
-                        + WomanShopSalesDef.USER_AGES.name() + " Text)"
-        );
-        Log.d("debug", "SingleSales Table onCreate");
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// DBバージョンアップ時のデータ移行を実装
+		if (oldVersion < newVersion) {
+			if (oldVersion == 1) {
+				db.execSQL("alter table " + WS_SALES_ORDER_TABLE_NAME
+						+ " add " + WomanShopSalesOrderDef.DISCOUNT.name() + " Real" );
 
-        db.execSQL("Create Table " + WS_SALES_ORDER_TABLE_NAME + " ("
-                        + "_id Integer Primary Key, "
-                        + WomanShopSalesOrderDef.PRODUCT_CODE.name() + " Text, "
-                        + WomanShopSalesOrderDef.PRODUCT_NAME.name() + " Text, "
-                        + WomanShopSalesOrderDef.CATEGORY_NAME.name() + " Text, "
-                        + WomanShopSalesOrderDef.PURCHASE_PRICE.name() + " Real, "
-                        + WomanShopSalesOrderDef.UNIT_PRICE.name() + " Real, "
-                        + WomanShopSalesOrderDef.QTY.name() + " Integer, "
-                        + WomanShopSalesOrderDef.DISCOUNT.name() + " Real, "
-                        + WomanShopSalesOrderDef.SINGLE_SALES_ID.name() + " Integer)"
-        );
-        Log.d("debug", "SingleSalesOrder table onCreate");
-    }
+				WomanShopSalesIOManager womanShopSalesIoManager = WomanShopSalesIOManager.getInstance();
+				ArrayList<SingleSalesRecord> sales = womanShopSalesIoManager.searchAll();
+				for (SingleSalesRecord sale : sales) {
+					if (sale.getDiscountValue() != 0) {
+						sale.calcDiscountAllocation();
+						ArrayList<Order> orders = sale.getAllOrders();
+						for (Order order : orders) {
+							double discount = order.getDiscount();
+							ContentValues value = new ContentValues();
+							//FIXME Where 句の条件のために Sales のID を取得できるようにする
+							String[] whereArgs = {"", order.getProductCode()};
+							value.put(WomanShopSalesOrderDef.DISCOUNT.name(), discount);
+							db.update(WS_SALES_ORDER_TABLE_NAME, value,
+									WomanShopSalesOrderDef.SINGLE_SALES_ID.name() + " = ? and "
+											+ WomanShopSalesOrderDef.PRODUCT_CODE.name() + " = ?", whereArgs);
+							;
+						}
+					}
+				}
 
-    @Override
-    public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
-    }
+			}
+		}
+	}
 
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL("Create Table " + WS_SALES_TABLE_NAME + " ("
+						+ "_id Integer Primary Key, "
+						+ WomanShopSalesDef.DATE.name() + " Text, "
+						+ WomanShopSalesDef.DISCOUNT.name() + " Real, "
+						+ WomanShopSalesDef.USER_AGES.name() + " Text)"
+		);
+		Log.d("debug", "SingleSales Table onCreate" );
+
+		db.execSQL("Create Table " + WS_SALES_ORDER_TABLE_NAME + " ("
+						+ "_id Integer Primary Key, "
+						+ WomanShopSalesOrderDef.PRODUCT_CODE.name() + " Text, "
+						+ WomanShopSalesOrderDef.PRODUCT_NAME.name() + " Text, "
+						+ WomanShopSalesOrderDef.CATEGORY_NAME.name() + " Text, "
+						+ WomanShopSalesOrderDef.PURCHASE_PRICE.name() + " Real, "
+						+ WomanShopSalesOrderDef.UNIT_PRICE.name() + " Real, "
+						+ WomanShopSalesOrderDef.QTY.name() + " Integer, "
+						+ WomanShopSalesOrderDef.DISCOUNT.name() + " Real, "
+						+ WomanShopSalesOrderDef.SINGLE_SALES_ID.name() + " Integer)"
+		);
+		Log.d("debug", "SingleSalesOrder table onCreate" );
+	}
 }
